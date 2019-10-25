2b0d6abe6169e3d0f098d3854ffae53544b4ac9c
Carl Ortutay <carlorty@gmail.com>
2019-10-25 18:56:37 -0400

    blogful complete

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO
+  blogful_articles (title, date_published, content)
+VALUES
+  ('Hong Kong',         DEFAULT,  'scelerisque mollis. Phasellus'),
+  ('Palau',             DEFAULT,  'tristique pharetra. Quisque'),
+  ('Congo',             DEFAULT,  'Suspendisse ac metus'),
+  ('Canada',            DEFAULT,  'eget lacus. Mauris'),
+  ('Saint Barthélemy',  DEFAULT,  'eu nulla at'),
+  ('Jersey',            DEFAULT,  'diam. Duis mi'),
+  ('Falkland Islands',  DEFAULT,  'cursus. Nunc mauris'),
+  ('Spain',             DEFAULT,  'turpis. Aliquam adipiscing'),
+  ('Grenada',           DEFAULT,  'est ac facilisis'),
+  ('Comoros',           DEFAULT,  'Sed neque. Sed'),
+  ('Iceland',           DEFAULT,  'aliquet odio. Etiam'),
+  ('Madagascar',        DEFAULT,  'odio. Aliquam vulputate'),
+  ('Ireland',           DEFAULT,  'rutrum magna. Cras'),
+  ('United States',     DEFAULT,  'eget, ipsum. Donec'),
+  ('Armenia',           DEFAULT,  'nec quam. Curabitur'),
+  ('Norfolk Island',    DEFAULT,  'tellus id nunc'),
+  ('Gambia',            DEFAULT,  'egestas. Duis ac'),
+  ('Cape Verde',        DEFAULT,  'euismod et, commodo'),
+  ('Marshall Islands',  DEFAULT,  'auctor, velit eget'),
+  ('Bhutan',            DEFAULT,  'consequat purus. Maecenas');
